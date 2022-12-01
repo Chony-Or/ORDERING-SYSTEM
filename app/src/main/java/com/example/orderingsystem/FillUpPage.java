1291e43dd3c0c81ea731a6a036fe787b2d4e6cc5
Chony-Or <chonyor76@gmail.com>
2022-12-02 02:18:30 +0800

    Update working specific category new interface in homepage

---
+++ app/src/main/java/com/example/orderingsystem/FillUpPage.java
@@ -0,0 +1,14 @@
+package com.example.orderingsystem;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class FillUpPage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_fill_up_page);
+    }
+}
