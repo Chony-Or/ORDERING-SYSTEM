ba2d243cdb44d68ca8c513cff43b2274345539f6
Chony-Or <chonyor76@gmail.com>
2023-04-09 22:24:02 +0800

    add new notification area (prototype not working pa)

---
+++ app/src/main/java/com/example/orderingsystem/NotificationAdapter.java
@@ -0,0 +1,4 @@
+package com.example.orderingsystem;
+
+public class NotificationAdapter {
+}
---
+++ app/src/main/java/com/example/orderingsystem/NotificationData.java
@@ -0,0 +1,80 @@
+package com.example.orderingsystem;
+
+public class NotificationData {
+
+    private Integer product_id;
+    private String product_name;
+    private Double product_price;
+    private Integer product_stock;
+    private String product_details;
+    private String product_code;
+    private String product_picture;
+
+
+
+
+    public NotificationData(Integer product_id, String product_name, Double product_price, Integer product_stock, String product_details, String product_code, String product_picture) {
+        this.product_id = product_id;
+        this.product_name = product_name;
+        this.product_price = product_price;
+        this.product_stock = product_stock;
+        this.product_details = product_details;
+        this.product_code = product_code;
+        this.product_picture = product_picture;
+    }
+
+    public String getProduct_details() {
+        return product_details;
+    }
+
+    public void setProduct_details(String product_details) {
+        this.product_details = product_details;
+    }
+    public Integer getProduct_id() {
+        return product_id;
+    }
+
+    public void setProduct_id(Integer product_id) {
+        this.product_id = product_id;
+    }
+
+    public String getProduct_name() {
+        return product_name;
+    }
+
+    public void setProduct_name(String product_name) {
+        this.product_name = product_name;
+    }
+
+    public Double getProduct_price() {
+        return product_price;
+    }
+
+    public void setProduct_price(Double product_price) {
+        this.product_price = product_price;
+    }
+
+    public Integer getProduct_stock() {
+        return product_stock;
+    }
+
+    public void setProduct_stock(Integer product_stock) {
+        this.product_stock = product_stock;
+    }
+
+    public String getProduct_code() {
+        return product_code;
+    }
+
+    public void setProduct_code(String product_code) {
+        this.product_code = product_code;
+    }
+
+    public String getProduct_picture() {
+        return product_picture;
+    }
+
+    public void setProduct_picture(String product_picture) {
+        this.product_picture = product_picture;
+    }
+}
